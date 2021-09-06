29b0bd8792da2646cb631ea52161114e417562d4
Juan Buitrago <juanbuitrago2002@outlook.com>
2021-09-06 15:21:50 -0500

    nuevo icono estilos payment

--- src/container/Payment.jsx
+++ src/container/Payment.jsx
@@ -3,6 +3,8 @@ import { useHistory } from 'react-router';
 import AppContext from '../context/AppContext';
 import { PayPalButton } from 'react-paypal-button-v2';
 import { handleSumTotal } from '../utils/index';
+import { FaCcPaypal } from 'react-icons/fa';
+import { SiCashapp } from 'react-icons/si';
 
 import '../styles/components/Payment.css';
 import '../styles/components/OpcionesPago.css';
@@ -17,97 +19,72 @@ export default function Payment() {
   return (
     <div className="Payment">
       <Bill />
-
       <div className="pagos">
         <h2>¿Cómo quieres pagar?</h2>
-        <ul className="opciones-pago">
-          <li className="opcion-pago">
-            <div>
-              <label className="ui-label">
-                <div className="ui-element">
-                  <input className="ui-radio__input" type="radio" />
-                </div>
-                <div className="ui-text">
-                  <span>💵</span>
-                  <span>Efectivo</span>
-                </div>
-              </label>
-            </div>
-          </li>
-          <li className="opcion-pago">
-            <div>
-              <label className="ui-label">
-                <div className="ui-element">
-                  <input className="ui-radio__input" type="radio" />
-                </div>
-                <div className="ui-text">
-                  <span>💵</span>
-                  <span>Tarjeta de Crédito - Débito (Presencial)</span>
-                </div>
-              </label>
-            </div>
-          </li>
-          <li className="opcion-pago">
-            <div>
-              <label className="ui-label">
-                <div className="ui-element">
-                  <input className="ui-radio__input" type="radio" />
-                </div>
-                <div className="ui-text">
-                  <span>💵</span>
-                  <span>Paypal</span>
-                </div>
-              </label>
-            </div>
-          </li>
-          <li className="opcion-pago">
-            <div>
-              <label className="ui-label">
-                <div className="ui-element">
-                  <input className="ui-radio__input" type="radio" />
-                </div>
-                <div className="ui-text">
-                  <span>💵</span>
-                  <span>Tarjeta Crédito - Débito 🟢(linea)</span>
-                </div>
-              </label>
-            </div>
-          </li>
-        </ul>
+        <h3>{`Total a Pagar: $${handleSumTotal(cart)}`}</h3>
+        <div className="menu-pagos">
+          <ul>
+            <li className="ui-text">
+              <div className="opciones-pago">
+                <span>Efectivo</span>
+                <span>
+                  <SiCashapp />
+                </span>
+              </div>
+              <div className="x">
+                <button className="button-pago" type="button">
+                  Pagar en Efectvo{' '}
+                </button>
+              </div>
+            </li>
+            <li className="ui-text">
+              <div className="opciones-pago">
+                <span>Paypal - Crédito - Débito</span>
+                <span>
+                  <FaCcPaypal />
+                </span>
+              </div>
+              <div className="x">
+                <PayPalButton
+                  createOrder={(data, actions, error) => {
+                    return actions.order.create({
+                      purchase_units: [
+                        {
+                          intent: 'CAPTURE',
+                          amount: {
+                            currency_code: 'USD',
+                            value: Total,
+                          },
+                        },
+                      ],
+                    });
+                  }}
+                  onSuccess={(details, data) => {
+                    const newOrder = {
+                      buyer,
+                      payment: details,
+                      order: data,
+                    };
+                    history.push('/checkout/success');
+                    addNewOrder(newOrder);
+                  }}
+                  options={{
+                    clientId: process.env.CLIENT_ID_PP,
+                  }}
+                  catchError={(error) =>
+                    alert(
+                      `Parece que hubo un error ${error} Vuelva a intentar.`
+                    )
+                  }
+                  onCancel={(data) =>
+                    alert('Parece que hubo un error. Vuelva a intentar.')
+                  }
+                />
+              </div>
+            </li>
+          </ul>
+        </div>
       </div>
-      <PayPalButton
-        createOrder={(data, actions, error) => {
-          return actions.order.create({
-            purchase_units: [
-              {
-                intent: 'CAPTURE',
-                amount: {
-                  currency_code: 'USD',
-                  value: Total,
-                },
-              },
-            ],
-          });
-        }}
-        onSuccess={(details, data) => {
-          const newOrder = {
-            buyer,
-            payment: details,
-            order: data,
-          };
-          history.push('/checkout/success');
-          addNewOrder(newOrder);
-        }}
-        options={{
-          clientId: process.env.CLIENT_ID_PP,
-        }}
-        catchError={(error) =>
-          alert(`Parece que hubo un error ${error} Vuelva a intentar.`)
-        }
-        onCancel={(data) =>
-          alert('Parece que hubo un error. Vuelva a intentar.')
-        }
-      />
     </div>
   );
 }
